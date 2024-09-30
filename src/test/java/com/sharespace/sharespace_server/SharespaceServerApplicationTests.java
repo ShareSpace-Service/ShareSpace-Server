a31438670fc64f3430b0fb42415a490673e113cf
이용학 <leeyonghak10@gmail.com>
2024-09-30 15:09:51 +0900

    initialize commit

---
+++ src/test/java/com/sharespace/sharespace_server/SharespaceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharespace.sharespace_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharespaceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
