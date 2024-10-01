fa2cd67daad2d46ca66b6f7cf5e6a3052191c1a5
thereisname <happycm1210@naver.com>
2024-10-01 16:22:09 +0900

    #5 [FEAT]-Place Entity 설계
    Place Entity 설계 및 Category Enum class 추가

---
+++ src/main/java/com/sharespace/sharespace_server/enums/Category.java
@@ -0,0 +1,22 @@
+package com.sharespace.sharespace_server.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Category {
+	CATEGORY_SMALL("Small"),
+	CATEGORY_MEDIUM("Medium"),
+	CATEGORY_LARGE("Large");
+	private String value;
+
+	public static Category fromValue(String value) {
+		for (Category category : Category.values()) {
+			if (category.getValue().equals(value)) {
+				return category;
+			}
+		}
+		throw new IllegalArgumentException("Unknown Category: " + value);
+	}
+}
--- src/main/java/com/sharespace/sharespace_server/place/entity/Place.java
+++ src/main/java/com/sharespace/sharespace_server/place/entity/Place.java
@@ -1,4 +1,51 @@
 package com.sharespace.sharespace_server.place.entity;
 
+import com.sharespace.sharespace_server.enums.Category;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "place", schema = "sharespace")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Place {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
+
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "user_id", nullable = false)
+	// private User user;
+
+	@Column(name = "title", nullable = false, length = 50)
+	private String title;
+
+	@Enumerated(EnumType.STRING)
+	private Category category;
+
+	@Column(name = "period", nullable = false)
+	private Integer period;
+
+	@Column(name = "description", length = 100)
+	private String description;
+
+	@Column(name = "image_url", nullable = false)
+	private String imageUrl;
 }
