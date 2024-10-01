408d3fc119feb1bca85761c41dcb23865fc146d6
yunhyungNAM <iys04003@naver.com>
2024-10-01 16:06:51 +0900

    #4 [FEAT]-User Entity 설계 및 권한을 위한 Enum 클래스 생성

--- src/main/java/com/sharespace/sharespace_server/user/entity/User.java
+++ src/main/java/com/sharespace/sharespace_server/user/entity/User.java
@@ -1,4 +1,72 @@
 package com.sharespace.sharespace_server.user.entity;
 
-public class User {
+import com.sharespace.sharespace_server.enums.Role;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user", schema = "sharespace")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nick_name", nullable = false, length = 50)
+    private String nickName;
+
+    @Column(name = "email", nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Column(name = "image", columnDefinition = "TEXT")
+    private String image;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 10)
+    private Role role;
+
+    @Column(name = "password", nullable = false, length = 255)
+    private String password;
+
+    @Column(name = "lock_time")
+    private LocalDateTime lockTime;
+
+    @Column(name = "failed_attempts", nullable = false)
+    private int failedAttempts = 0;
+
+    @Column(name = "location", nullable = false, length = 50)
+    private String location;
+
+    @Column(name = "latitude", nullable = false)
+    private Double latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private Double longitude;
+
+    @Column(name = "email_validated", nullable = false)
+    private Boolean emailValidated = false;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getEmail();
+    }
+
 }
