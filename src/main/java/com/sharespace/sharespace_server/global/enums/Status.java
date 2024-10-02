70f4b89af90c76095dee79ef6341a85e55dedb30
dradradre <leeyonghak13@gmail.com>
2024-10-02 13:25:09 +0900

    #3 [CHORE]-Status의 ENUM Field 값들을 대문자로 수정

--- src/main/java/com/sharespace/sharespace_server/global/enums/Status.java
+++ src/main/java/com/sharespace/sharespace_server/global/enums/Status.java
@@ -1,10 +1,11 @@
 package com.sharespace.sharespace_server.global.enums;
 
 public enum Status {
-	Unassigned,
-	Requested,
-	Rejected,
-	Pending,
-	Stored,
-	Completed
+	UNASSIGNED, // 미배정 (장소 선택 안 하고 물품만 등록함)
+	REQUESTED, // 요청됨 (물품 등록 + 장소 선택까지 함)
+	REJECTED, // 반려됨 (호스트 측에서 거절)
+	PENDING, // (보관) 대기중 -> 호스트가 장소 선택 후, 실질적인 보관은 하지 않은 대기 상태
+
+	STORED, // 보관중
+	COMPLETED // 완료됨
 }
