fe530e82e504b4a6bd94ee8c3422a6b1374ffe23
yunhyungNAM <iys04003@naver.com>
2024-10-02 16:18:49 +0900

    #4 [FEAT] - User Repository JPA 상속 및 interface 전환

--- src/main/java/com/sharespace/sharespace_server/user/repository/UserRepository.java
+++ src/main/java/com/sharespace/sharespace_server/user/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.sharespace.sharespace_server.user.repository;
 
-public class UserRepository {
+import com.sharespace.sharespace_server.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
