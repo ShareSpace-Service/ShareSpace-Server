c62d594f285b650186a6d901519ca43911ac1b16
dradradre <leeyonghak13@gmail.com>
2024-10-02 13:25:46 +0900

    #3 [CHORE]-MatchingRepository가 JPARepository를 상속받게끔 수정

--- src/main/java/com/sharespace/sharespace_server/matching/repository/MatchingRepository.java
+++ src/main/java/com/sharespace/sharespace_server/matching/repository/MatchingRepository.java
@@ -1,4 +1,10 @@
 package com.sharespace.sharespace_server.matching.repository;
 
-public class MatchingRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sharespace.sharespace_server.matching.entity.Matching;
+
+@Repository
+public interface MatchingRepository extends JpaRepository<Matching, Long> {
 }
