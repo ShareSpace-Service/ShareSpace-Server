53750f7302e52fe8b44f65f4c66858902bd141a3
thereisname <happycm1210@naver.com>
2024-10-03 14:42:18 +0900

    #7 [CHORE]-Note에서 matching, product, place repository jpa 추가

--- src/main/java/com/sharespace/sharespace_server/place/repository/PlaceRepository.java
+++ src/main/java/com/sharespace/sharespace_server/place/repository/PlaceRepository.java
@@ -1,5 +1,6 @@
 package com.sharespace.sharespace_server.place.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,5 @@ import com.sharespace.sharespace_server.place.entity.Place;
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 	Optional<Place> findById(Long placeId);
 	Optional<Place> findByUserId(Long userId);
+	List<Place> findAllByUserId(Long userId);
 }
