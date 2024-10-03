52a686b92070d1b8aff7ed06f66ef77f9f6a8a16
thereisname <happycm1210@naver.com>
2024-10-03 20:06:05 +0900

    #38 [REFACTOR]-불필요 메서드 하나로 통일 및 주석 추가

--- src/main/java/com/sharespace/sharespace_server/global/utils/DistanceUtils.java
+++ src/main/java/com/sharespace/sharespace_server/global/utils/DistanceUtils.java
@@ -3,7 +3,16 @@ package com.sharespace.sharespace_server.global.utils;
 public class DistanceUtils {
 	private static final double EARTH_RADIUS = 6371000; // 지구 반지름 (미터)
 
-	public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+	/**
+	 * 게스트, 호스트 각각의 위도, 경도를 받아 두 사람의 거리를 계산하는 로직
+	 *
+	 * @param lat1 Guest Latitude(위도)
+	 * @param lon1 Guest Longitude(경도)
+	 * @param lat2 Host Latitude(위도)
+	 * @param lon2 Host Longitude(경도)
+	 * @return 거리를 계산 후 정수의 값으로 반올림한 값
+	 */
+	public static int calculateDistance(double lat1, double lon1, double lat2, double lon2) {
 		double dLat = Math.toRadians(lat2 - lat1);
 		double dLon = Math.toRadians(lon2 - lon1);
 
@@ -12,11 +21,6 @@ public class DistanceUtils {
 			* Math.sin(dLon / 2) * Math.sin(dLon / 2);
 		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-		return EARTH_RADIUS * c; // 거리 (미터)
-	}
-
-	public static int calculateRoundedDistance(double lat1, double lon1, double lat2, double lon2) {
-		double distance = calculateDistance(lat1, lon1, lat2, lon2);
-		return (int) Math.round(distance); // 거리 반올림 후 정수로 변환
+		return (int) Math.round(EARTH_RADIUS * c); // 거리 (미터)
 	}
 }
