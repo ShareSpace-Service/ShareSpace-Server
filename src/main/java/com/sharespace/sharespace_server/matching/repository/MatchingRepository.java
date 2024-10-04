87b67293d200454a9e96a0faff159035f8a2b9ea
dradradre <leeyonghak13@gmail.com>
2024-10-04 17:23:25 +0900

    #46 [FEAT]-MatchingRepository에 메서드 추가
    - productId와 placeId로 Matching을 찾는 JPA 메서드

--- src/main/java/com/sharespace/sharespace_server/matching/repository/MatchingRepository.java
+++ src/main/java/com/sharespace/sharespace_server/matching/repository/MatchingRepository.java
@@ -1,6 +1,7 @@
 package com.sharespace.sharespace_server.matching.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,4 +14,6 @@ public interface MatchingRepository extends JpaRepository<Matching, Long> {
 	List<Matching> findAllByPlaceIdInAndStatusIn(List<Long> places, List<Status> statuses);
 	List<Matching> findAllByProductIdInAndStatusIn(List<Long> product, List<Status> statuses);
 	Matching findByProductIdInAndPlaceId(List<Long> product, Long place);
+
+	Optional<Matching> findByProductIdAndPlaceId(Long productId, Long placeId);
 }
