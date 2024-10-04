e29fb69cf3c0cedc34a67c848911fc07af2aca62
dradradre <leeyonghak13@gmail.com>
2024-10-04 17:24:13 +0900

    #46 [REFACTOR]-Status Enum에 불필요한 공백 제거
    - productId와 placeId로 Matching을 찾는 JPA 메서드

--- src/main/java/com/sharespace/sharespace_server/global/enums/Status.java
+++ src/main/java/com/sharespace/sharespace_server/global/enums/Status.java
@@ -5,7 +5,6 @@ public enum Status {
 	REQUESTED, // 요청됨 (물품 등록 + 장소 선택까지 함)
 	REJECTED, // 반려됨 (호스트 측에서 거절)
 	PENDING, // (보관) 대기중 -> 호스트가 장소 선택 후, 실질적인 보관은 하지 않은 대기 상태
-
 	STORED, // 보관중
 	COMPLETED // 완료됨
 }
