0169e7435af6f997870a4fe460779bcdf0d459a8
yunhyungNAM <iys04003@naver.com>
2024-10-04 16:16:21 +0900

    #48 [CHORE] - 모든경로에 대한 인가 처리 임시 허용

--- src/main/java/com/sharespace/sharespace_server/global/config/SecurityConfig.java
+++ src/main/java/com/sharespace/sharespace_server/global/config/SecurityConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -27,7 +25,7 @@ public class SecurityConfig{
         return http
                 .csrf(((auth) -> auth.disable()))
                 .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/user/register").permitAll() // 회원가입 API는 인증 없이 접근 가능
+                .requestMatchers("/**").permitAll() // 회원가입 API는 인증 없이 접근 가능
                 .anyRequest().authenticated() // 그 외 요청은 인증 필요
                 )
                 .build();
