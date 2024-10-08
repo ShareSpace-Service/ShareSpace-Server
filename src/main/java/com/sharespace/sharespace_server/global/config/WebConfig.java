6b94f519bb26823c2e87f5a354ac2c58f79e3136
thereisname <happycm1210@naver.com>
2024-10-08 17:22:19 +0900

    [FIX]-WebConfig 추가로 CORS 해결

---
+++ src/main/java/com/sharespace/sharespace_server/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.sharespace.sharespace_server.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**") // 모든 경로에 대해 CORS 설정을 허용
+			.allowedOrigins("http://localhost:5173") // 허용할 도메인
+			.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+			.allowedHeaders("*") // 모든 헤더 허용
+			.allowCredentials(true) // 자격 증명 허용
+			.maxAge(3600); // preflight 요청을 캐싱할 시간(초)
+	}
+}
