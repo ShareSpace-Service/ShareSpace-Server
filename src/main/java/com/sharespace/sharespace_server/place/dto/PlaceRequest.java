f90a50a9419d6b34f004313777872319d609e8f4
thereisname <happycm1210@naver.com>
2024-10-10 23:21:29 +0900

    #71 [REFACTOR]-validation 추가

--- src/main/java/com/sharespace/sharespace_server/place/dto/PlaceRequest.java
+++ src/main/java/com/sharespace/sharespace_server/place/dto/PlaceRequest.java
@@ -6,6 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.sharespace.sharespace_server.global.enums.Category;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -14,9 +15,13 @@ import lombok.Setter;
 @Getter
 @RequiredArgsConstructor
 public class PlaceRequest {
+	@NotNull(message = "Title을 입력해주세요")
 	private String title;
+	@NotNull(message = "Category를 선택해주세요")
 	private Category category;
+	@NotNull(message = "최대 보관 기간을 입력해주세요")
 	private Integer period;
+	@NotNull(message = "이미지를 선택해주세요")
 	private List<MultipartFile> imageUrl;
 	private String description;
 }
