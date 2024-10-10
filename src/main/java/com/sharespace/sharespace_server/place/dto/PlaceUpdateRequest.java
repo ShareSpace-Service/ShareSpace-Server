c48f2a93bb8e451b4b0408c32bcd0bdf1914cf04
thereisname <happycm1210@naver.com>
2024-10-10 23:19:00 +0900

    #71 [FEAT]-이미지 업로드 및 수정 추가

--- src/main/java/com/sharespace/sharespace_server/place/dto/PlaceUpdateRequest.java
+++ src/main/java/com/sharespace/sharespace_server/place/dto/PlaceUpdateRequest.java
@@ -1,7 +1,12 @@
 package com.sharespace.sharespace_server.place.dto;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.sharespace.sharespace_server.global.enums.Category;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -10,10 +15,15 @@ import lombok.Setter;
 @Getter
 @RequiredArgsConstructor
 public class PlaceUpdateRequest {
+	@NotNull(message = "Title을 입력해주세요")
 	private String title;
+	@NotNull(message = "Category를 선택해주세요")
 	private Category category;
+	@NotNull(message = "최대 보관 기간을 입력해주세요")
 	private Integer period;
-	private String image_url;
-	private String description;
+	@NotNull(message = "위치 정보를 입력해주세요")
 	private String location;
+	private List<MultipartFile> newImageUrl;
+	private List<String> deleteImageUrl;
+	private String description;
 }
--- src/main/java/com/sharespace/sharespace_server/place/service/PlaceService.java
+++ src/main/java/com/sharespace/sharespace_server/place/service/PlaceService.java
@@ -1,23 +1,24 @@
 package com.sharespace.sharespace_server.place.service;
 
 import static com.sharespace.sharespace_server.global.utils.LocationTransform.*;
-import static com.sharespace.sharespace_server.global.utils.S3ImageUpload.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.sharespace.sharespace_server.global.enums.Category;
 import com.sharespace.sharespace_server.global.exception.CustomRuntimeException;
+import com.sharespace.sharespace_server.global.exception.error.ImageException;
 import com.sharespace.sharespace_server.global.exception.error.PlaceException;
 import com.sharespace.sharespace_server.global.exception.error.ProductException;
 import com.sharespace.sharespace_server.global.exception.error.UserException;
 import com.sharespace.sharespace_server.global.response.BaseResponse;
 import com.sharespace.sharespace_server.global.response.BaseResponseService;
 import com.sharespace.sharespace_server.global.utils.LocationTransform;
+import com.sharespace.sharespace_server.global.utils.S3ImageUpload;
 import com.sharespace.sharespace_server.place.dto.PlaceDetailResponse;
 import com.sharespace.sharespace_server.place.dto.PlaceRequest;
 import com.sharespace.sharespace_server.place.dto.PlaceUpdateRequest;
@@ -42,40 +43,7 @@ public class PlaceService {
 	private final PlaceRepository placeRepository;
 	private final ProductRepository productRepository;
 	private final LocationTransform locationTransform;
-
-	/**
-	 * 주어진 장소 정보를 게스트 사용자와 함께 PlacesResponse 객체로 매핑합니다.
-	 *
-	 * @param place 장소 정보
-	 * @param guest 게스트 사용자
-	 * @return 매핑된 PlacesResponse 객체
-	 * @throws CustomRuntimeException 호스트 사용자가 존재하지 않을 경우 발생
-	 */
-	private PlacesResponse mapToPlacesResponse(Place place, User guest) {
-		User host = userRepository.findById(place.getUser().getId())
-			.orElseThrow(() -> new CustomRuntimeException(UserException.MEMBER_NOT_FOUND));
-
-		Integer distance = calculateDistance(
-			guest.getLatitude(), guest.getLongitude(), host.getLatitude(), host.getLongitude()
-		);
-
-		// imageUrl 필드를 다중 이미지 배열로 변환
-		List<String> imageUrls = Arrays.asList(place.getImageUrl().split(","));
-
-		return PlacesResponse.builder()
-			.placeId(place.getId())
-			.title(place.getTitle())
-			.category(place.getCategory())
-			.imageUrls(imageUrls) // 다중 이미지 URL 배열 설정
-			.distance(distance)
-			.build();
-	}
-
-	// user 정보 찾기
-	private User findByUser(Long userId) {
-		return userRepository.findById(userId)
-			.orElseThrow(() -> new CustomRuntimeException(UserException.MEMBER_NOT_FOUND));
-	}
+	private final S3ImageUpload s3ImageUpload;
 
 	@Transactional
 	public BaseResponse<List<PlacesResponse>> getAllPlaces() {
@@ -106,7 +74,8 @@ public class PlaceService {
 
 	@Transactional
 	public BaseResponse<PlaceDetailResponse> getPlaceDetail(Long placeId) {
-		Place place = placeRepository.findById(placeId).orElseThrow(() -> new CustomRuntimeException(PlaceException.PLACE_NOT_FOUND));
+		Place place = placeRepository.findById(placeId)
+			.orElseThrow(() -> new CustomRuntimeException(PlaceException.PLACE_NOT_FOUND));
 
 		PlaceDetailResponse placeDetailResponse = PlaceDetailResponse.from(place);
 
@@ -115,13 +84,7 @@ public class PlaceService {
 
 	@Transactional
 	public BaseResponse<String> createPlace(PlaceRequest placeRequest) {
-		User user = findByUser(1L);
-
-		// request 항목 중 하나라도 빈 값이 들어온 경우 예외 처리
-		if(placeRequest.getTitle().isEmpty() || placeRequest.getPeriod() == null
-			|| placeRequest.getCategory() == null || placeRequest.getDescription().isEmpty()) {
-			throw new CustomRuntimeException(PlaceException.PLACE_REQUIRED_FIELDS_EMPTY);
-		}
+		User user = findByUser(5L);
 
 		// 이미 User Id의 값으로 장소가 만들어져 있는 경우 예외 처리
 		placeRepository.findByUserId(user.getId())
@@ -129,8 +92,13 @@ public class PlaceService {
 				throw new CustomRuntimeException(PlaceException.PLACE_ALREADY_EXISTS);
 			});
 
+		// 이미지 Request 검증
+		if (!s3ImageUpload.isRequestImages(placeRequest.getImageUrl())) {
+			throw new CustomRuntimeException(ImageException.IMAGE_REQUIRED_FIELDS_EMPTY);
+		};
+
 		// 다중 이미지 S3에 업로드
-		String combinedImageUrls = uploadMultipleFiles(placeRequest.getImageUrl(), "place/" +user.getId());
+		List<String> combinedImageUrls = s3ImageUpload.uploadMultipleFiles(placeRequest.getImageUrl(), "place/" +user.getId());
 
 		Place place = Place.builder()
 			.title(placeRequest.getTitle())
@@ -138,7 +106,7 @@ public class PlaceService {
 			.period(placeRequest.getPeriod())
 			.description(placeRequest.getDescription())
 			.category(placeRequest.getCategory())
-			.imageUrl(combinedImageUrls)
+			.imageUrl(combinedImageUrls.toString())
 			.build();
 
 		placeRepository.save(place);
@@ -148,33 +116,83 @@ public class PlaceService {
 
 	@Transactional
 	public BaseResponse<String> updatePlace(PlaceUpdateRequest placeRequest) {
-		// request 항목 중 하나라도 빈 값이 들어온 경우 예외 처리
-		if(placeRequest.getTitle().isEmpty() || placeRequest.getPeriod() == null
-			|| placeRequest.getCategory() == null || placeRequest.getDescription().isEmpty()
-			|| placeRequest.getLocation().isEmpty()) {
-			throw new CustomRuntimeException(PlaceException.PLACE_REQUIRED_FIELDS_EMPTY);
-		}
-
 		User user = findByUser(1L);
-
-		Map<String, Double> coordinates = locationTransform.getCoordinates(placeRequest.getLocation());
-
-		user.setLocation(placeRequest.getLocation());
-		user.setLatitude(coordinates.get("latitude"));
-		user.setLongitude(coordinates.get("longitude"));
 
 		Place place = placeRepository.findByUserId(user.getId())
 			.orElseThrow(() -> new CustomRuntimeException(PlaceException.PLACE_NOT_FOUND));
 
-		place.setTitle(placeRequest.getTitle());
-		place.setPeriod(placeRequest.getPeriod());
-		place.setCategory(placeRequest.getCategory());
-		place.setDescription(placeRequest.getDescription());
-		place.setImageUrl(placeRequest.getImage_url());
+		List<String> updateImages = s3ImageUpload.updateImages(
+			placeRequest.getDeleteImageUrl(), placeRequest.getNewImageUrl(),
+			"place/" + user.getId(), place.getImageUrl());
 
+		// 이미지 업데이트 처리
+		place.setImageUrl(String.join(",", updateImages));
+
+		// 사용자 위치 업데이트
+		updateUserLocation(user, placeRequest);
+		// 장소 필드 및 이미지 URL 업데이트
+		updatePlaceFields(place, placeRequest, updateImages);
+
+		// 변경 사항 저장
 		userRepository.save(user);
 		placeRepository.save(place);
 
 		return baseResponseService.getSuccessResponse("장소 수정 성공!");
+	}
+
+	// task: 장소 정보를 게스트, 호스트 거리를 포함하여 PlacesResponse 객체로 변환
+	private PlacesResponse mapToPlacesResponse(Place place, User guest) {
+		User host = findByUser(place.getUser().getId());
+
+		Integer distance = calculateDistance(
+			guest.getLatitude(), guest.getLongitude(), host.getLatitude(), host.getLongitude()
+		);
+
+		return PlacesResponse.builder()
+			.placeId(place.getId())
+			.title(place.getTitle())
+			.category(place.getCategory())
+			.imageUrls(List.of(place.getImageUrl().split(","))) // 다중 이미지 URL 배열 설정
+			.distance(distance)
+			.build();
+	}
+
+	// task: user 정보 찾기
+	private User findByUser(Long userId) {
+		return userRepository.findById(userId)
+			.orElseThrow(() -> new CustomRuntimeException(UserException.MEMBER_NOT_FOUND));
+	}
+
+	// task: 장소 위치 변경 로직
+	private void updateUserLocation(User user, PlaceUpdateRequest placeRequest) {
+		// 사용자의 위치가 변경되었는지 확인
+		if (!user.getLocation().equals(placeRequest.getLocation())) {
+			// 새로운 위치에 대한 좌표 변환
+			Map<String, Double> coordinates = locationTransform.getCoordinates(placeRequest.getLocation());
+			// 사용자 위치 정보 및 좌표 업데이트
+			user.setLocation(placeRequest.getLocation());
+			user.setLatitude(coordinates.get("latitude"));
+			user.setLongitude(coordinates.get("longitude"));
+		}
+	}
+
+	// task: 장소 필드 업데이트
+	private void updatePlaceFields(Place place, PlaceUpdateRequest placeRequest, List<String> updateImages) {
+		// 필드별로 null 허용하며, 값이 변경되었을 때만 업데이트
+		if (!Objects.equals(place.getTitle(), placeRequest.getTitle())) {
+			place.setTitle(placeRequest.getTitle());
+		}
+		if (!Objects.equals(place.getPeriod(), placeRequest.getPeriod())) {
+			place.setPeriod(placeRequest.getPeriod());
+		}
+		if (!Objects.equals(place.getCategory(), placeRequest.getCategory())) {
+			place.setCategory(placeRequest.getCategory());
+		}
+		if (!Objects.equals(place.getDescription(), placeRequest.getDescription())) {
+			place.setDescription(placeRequest.getDescription());
+		}
+
+		// 이미지 URL 업데이트
+		place.setImageUrl(String.join(",", updateImages));
 	}
 }
