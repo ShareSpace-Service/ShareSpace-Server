641ec678c7789eaa20d944668ba295b1d18a7458
Dradradre <leeyonghak13@gmail.com>
2024-10-12 23:50:30 +0900

    #76 [FEAT]-Notification Repository에 findAllByUser 메서드 추가
    - 특정 유저 기준으로 알림 목록을 찾는 메서드 추가

--- src/main/java/com/sharespace/sharespace_server/notification/repository/NotificationRepository.java
+++ src/main/java/com/sharespace/sharespace_server/notification/repository/NotificationRepository.java
@@ -1,8 +1,12 @@
 package com.sharespace.sharespace_server.notification.repository;
 
+import com.sharespace.sharespace_server.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sharespace.sharespace_server.notification.entity.Notification;
 
+import java.util.List;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByUser(User user);
 }
