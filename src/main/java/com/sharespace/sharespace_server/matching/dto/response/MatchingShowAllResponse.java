8e170335f4cc1a4b26aa62d6d5fc4596c9693220
dradradre <leeyonghak13@gmail.com>
2024-10-15 14:24:00 +0900

    #87 [FEAT]-모든 Matching을 조회하는 Response DTO 추가

---
+++ src/main/java/com/sharespace/sharespace_server/matching/dto/response/MatchingShowAllResponse.java
@@ -0,0 +1,19 @@
+package com.sharespace.sharespace_server.matching.dto.response;
+
+import com.sharespace.sharespace_server.global.enums.Category;
+import com.sharespace.sharespace_server.global.enums.Status;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MatchingShowAllResponse {
+	private Long matchingId;
+	private String title;
+	private Category category;
+	private List<String> imageUrl; // product 이미지
+	private Status status;
+	private Integer distance;
+}
