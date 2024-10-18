6ec8aaa23f12401e379fc9a70dcfff96143b4e5c
dradradre <leeyonghak13@gmail.com>
2024-10-18 15:53:10 +0900

    #76 [FEAT]-findAllByUserId 메서드에 @EntityGraph 어노테이션 적용

--- src/main/java/com/sharespace/sharespace_server/product/repository/ProductRepository.java
+++ src/main/java/com/sharespace/sharespace_server/product/repository/ProductRepository.java
@@ -2,12 +2,16 @@ package com.sharespace.sharespace_server.product.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.sharespace.sharespace_server.product.entity.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	@EntityGraph(attributePaths = {"matchings.place"})
 	List<Product> findAllByUserId(Long userId);
 }
