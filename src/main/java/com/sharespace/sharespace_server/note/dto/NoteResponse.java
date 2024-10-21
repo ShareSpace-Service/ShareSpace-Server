b7e1412e9b2449a4c9cdcc92db83a730f1d48adc
thereisname <happycm1210@naver.com>
2024-10-21 15:21:41 +0900

    #78 [CHORE]-Response 생성을 각각의 클래스에서 담당하도록 책임분리

--- src/main/java/com/sharespace/sharespace_server/note/dto/NoteResponse.java
+++ src/main/java/com/sharespace/sharespace_server/note/dto/NoteResponse.java
@@ -1,5 +1,7 @@
 package com.sharespace.sharespace_server.note.dto;
 
+import com.sharespace.sharespace_server.note.entity.Note;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,4 +16,13 @@ public class NoteResponse {
 	private String title;
 	private String content;
 	private String sender;
+
+	public static NoteResponse toNoteResponse(Note note) {
+		return NoteResponse.builder()
+			.noteId(note.getId())
+			.title(note.getTitle())
+			.content(note.getContent())
+			.sender(note.getSender().getNickName())
+			.build();
+	}
 }
