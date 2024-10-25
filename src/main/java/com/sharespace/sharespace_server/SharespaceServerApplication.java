359e4ad37db8fb6e1c6872dcf62b96a09f93caad
yunhyungNAM <iys04003@naver.com>
2024-10-25 17:22:55 +0900

    #110 [Feat] - 매일 밤 자정 이메일 미인증 유저들 탈퇴처리 기능 추가

--- src/main/java/com/sharespace/sharespace_server/SharespaceServerApplication.java
+++ src/main/java/com/sharespace/sharespace_server/SharespaceServerApplication.java
@@ -2,8 +2,10 @@ package com.sharespace.sharespace_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SharespaceServerApplication {
 
 	public static void main(String[] args) {
