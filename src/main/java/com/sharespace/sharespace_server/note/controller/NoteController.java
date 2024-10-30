9e7d2fdff00444e4e04112c99fd14acd0c15c48f
thereisname <happycm1210@naver.com>
2024-10-30 16:19:09 +0900

    #78 [DOCS]-Note 관련 API에 주석 추가

--- src/main/java/com/sharespace/sharespace_server/note/controller/NoteController.java
+++ src/main/java/com/sharespace/sharespace_server/note/controller/NoteController.java
@@ -30,28 +30,33 @@ import lombok.extern.slf4j.Slf4j;
 public class NoteController {
 	private final NoteService noteService;
 
+	// task: 받은 쪽지 전체 조회
 	@GetMapping
 	public BaseResponse<List<NoteResponse>> getNote(HttpServletRequest httpRequest) {
 		Long userId = RequestParser.extractUserId(httpRequest);
 		return noteService.getAllNotes(userId);
 	}
 
+	// task: 쪽지 전송
 	@PostMapping
 	public  BaseResponse<String> createNote(@Valid @RequestBody NoteRequest noteRequest, HttpServletRequest httpRequest) {
 		Long userId = RequestParser.extractUserId(httpRequest);
 		return noteService.createNote(noteRequest, userId);
 	}
 
+	// task: 쪽지 삭제
 	@DeleteMapping
 	public BaseResponse<String> deleteNote(@RequestParam Long noteId) {
 		return noteService.deleteNote(noteId);
 	}
 
+	// task: 상세 쪽지 내용 조회
 	@GetMapping("/noteDetail")
 	public BaseResponse<NoteDetailResponse> getNoteDetail(@RequestParam Long noteId) {
 		return noteService.getNoteDetail(noteId);
 	}
 
+	// task: 수신자 대상 조회
 	@GetMapping("/available")
 	public BaseResponse<List<NoteSenderListResponse>> getSenderList(HttpServletRequest httpRequest) {
 		Long userId = RequestParser.extractUserId(httpRequest);
