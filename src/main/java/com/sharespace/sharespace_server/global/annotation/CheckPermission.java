ca1f383735963658589f2c2900d2f9db6075493a
dradradre <leeyonghak13@gmail.com>
2024-11-05 16:20:32 +0900

    #141 [FEAT]-Spring AOP 관련 클래스 추가 - `build.gradle` : Spring AOP 의존성 추가 - `AopConfig.java` : Spring AOP 기능을 활성화 시키기 위한 Configuration 클래스 파일 - `CheckPermission.java` : 권한 검증을 진행할 커스텀 어노테이션 - `PermissionAspect.java` : CheckPermission 어노테이션에서 동작할 Aspect를 정의한 클래스

---
+++ src/main/java/com/sharespace/sharespace_server/global/annotation/CheckPermission.java
@@ -0,0 +1,12 @@
+package com.sharespace.sharespace_server.global.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CheckPermission {
+	String[] roles();
+}
