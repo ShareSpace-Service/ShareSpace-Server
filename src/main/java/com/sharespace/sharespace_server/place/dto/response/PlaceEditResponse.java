8e888dbffc8952e8a7dca921be58349bd7401f18
thereisname <happycm1210@naver.com>
2024-11-14 17:09:35 +0900

    #143 [FEAT]-쪽지 발신일 기준으로 내림차순 정렬

---
+++ src/main/java/com/sharespace/sharespace_server/place/dto/response/PlaceEditResponse.java
@@ -0,0 +1,33 @@
+package com.sharespace.sharespace_server.place.dto.response;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.sharespace.sharespace_server.place.entity.Place;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class PlaceEditResponse {
+	private Long placeId;
+	private String location;
+	private String title;
+	private String category;
+	private Integer period;
+	private List<String> imageUrl;
+	private String description;
+
+	public static PlaceEditResponse of(Place place, String location) {
+		return PlaceEditResponse.builder()
+			.placeId(place.getId())
+			.location(location)
+			.title(place.getTitle())
+			.category(place.getCategory().toString())
+			.period(place.getPeriod())
+			.imageUrl(Arrays.asList(place.getImageUrl().split(",")))
+			.description(place.getDescription())
+			.build();
+	}
+}
